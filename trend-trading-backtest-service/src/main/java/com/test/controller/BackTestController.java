0fbb180621d370120b652ac01d6e4155b267f14c
tangxiaolan <2279777383@qq.com>
2020-08-20 19:42:33 +0800

    daima

--- trend-trading-backtest-service/src/main/java/com/test/controller/BackTestController.java
+++ trend-trading-backtest-service/src/main/java/com/test/controller/BackTestController.java
@@ -1,17 +1,26 @@
 package com.test.controller;
 
+import cn.hutool.core.util.StrUtil;
 import com.how2j.trend.pojo.Index;
 import com.how2j.trend.pojo.IndexData;
+import com.how2j.trend.pojo.Profit;
 import com.test.service.BackTestService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.swing.text.DateFormatter;
+import java.lang.reflect.Parameter;
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class BackTestController {
@@ -19,12 +28,63 @@ public class BackTestController {
     @Autowired
     private BackTestService service;
 
-    @GetMapping("/simulate/{code}")
+    @GetMapping("/simulate/{code}/{startDate}/{endDate}")
     @CrossOrigin
-    public Map<String, List<IndexData>> getIndexData(@PathVariable("code") String code){
+    public Map<String, Object> getIndexData(@PathVariable("code") String code,
+            @PathVariable("startDate") String startDate,    @PathVariable("endDate") String endDate){
         List<IndexData> list =  service.getIndexData(code);
-        HashMap<String, List<IndexData>> map = new HashMap();
+        list = filter(list, startDate, endDate);
+
+        String indexStartDate = list.get(0).getDate();
+        String indexEndDate = list.get(list.size()-1).getDate();
+
+        int ma = 20;
+        float sellRate = 0.95f;
+        float buyRate = 1.05f;
+        float serviceCharge = 0f;
+        Map<String,?>  stringMap  = service.simulate(ma, sellRate, buyRate, serviceCharge,list );
+        List<Profit> profts = (List<Profit>) stringMap.get("profits");
+        float year = service.getYear(list);
+        IndexData start = list.get(0);
+        IndexData end = list.get(list.size()-1);
+        float indexIncomeTotal = (end.getClosePoint() - start.getClosePoint())/start.getClosePoint();
+        float indexIncomeAnnual = (float) (Math.pow(end.getClosePoint()/start.getClosePoint(),1/year) -1);
+        float trendIncomeTotal = (profts.get(profts.size()-1).getValue() - profts.get(0).getValue())/profts.get(0).getValue();
+        float trendIncomeAnnual = (float) Math.pow(1+trendIncomeTotal, 1/year) - 1;
+
+        int winCount = (Integer) stringMap.get("winCount");
+        int lossCount = (Integer) stringMap.get("lossCount");
+        float avgWinRate = (Float) stringMap.get("avgWinRate");
+        float avgLossRate = (Float) stringMap.get("avgLossRate");
+
+
+        HashMap<String, Object> map = new HashMap();
         map.put("indexDatas", list);
+        map.put("indexStartDate", indexStartDate);
+        map.put("indexEndDate", indexEndDate);
+        map.put("profits",stringMap.get("profits"));
+        map.put("trades",stringMap.get("trades"));
+        map.put("years", year);
+        map.put("indexIncomeTotal", indexIncomeTotal);
+        map.put("indexIncomeAnnual", indexIncomeAnnual);
+        map.put("trendIncomeTotal", trendIncomeTotal);
+        map.put("trendIncomeAnnual", trendIncomeAnnual);
+
+        map.put("winCount", winCount);
+        map.put("lossCount", lossCount);
+        map.put("avgWinRate", avgWinRate);
+        map.put("avgLossRate", avgLossRate);
         return map;
+    }
+
+    private List<IndexData> filter(List<IndexData> list, String startDate, String endDate){
+        if(StrUtil.isBlankOrUndefined(startDate) || StrUtil.isBlankOrUndefined(endDate) ){
+            return list;
+        }
+        LocalDate localStartDate = LocalDate.parse(startDate,DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate localEndDate = LocalDate.parse(endDate,DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        List<IndexData> result = list.stream().filter(x -> LocalDate.parse(x.getDate(),DateTimeFormatter.ofPattern("yyyy-MM-dd")).compareTo(localStartDate) >= 0)
+                .filter(x -> LocalDate.parse(x.getDate(),DateTimeFormatter.ofPattern("yyyy-MM-dd")).compareTo(localEndDate) <= 0).collect(Collectors.toList());
+        return result;
     }
 }
--- trend-trading-backtest-service/src/main/java/com/test/service/BackTestService.java
+++ trend-trading-backtest-service/src/main/java/com/test/service/BackTestService.java
@@ -1,12 +1,16 @@
 package com.test.service;
 
 import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
 import com.how2j.trend.pojo.IndexData;
+import com.how2j.trend.pojo.Profit;
+import com.how2j.trend.pojo.Trade;
 import com.test.client.IndexDataClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.*;
 
 @Component
 public class BackTestService {
@@ -19,5 +23,136 @@ public class BackTestService {
         CollectionUtil.reverse(result);
         result.stream().forEach(x ->System.out.println(x.getDate()));
         return result;
+    }
+    public Map<String,Object> simulate(int ma, float sellRate, float buyRate, float serviceCharge,
+                                       List<IndexData> indexDatas){
+        List<Profit> Profits = new ArrayList<>();
+        List<Trade> trades = new ArrayList<>();
+
+        float initCash = 1000;
+        float cash = initCash;
+        float share = 0;
+        float value = 0;
+        float init = 0;
+
+        int winCount = 0;
+        float totalWinRate = 0;
+        float avgWinRate = 0;
+        float totalLossRate = 0;
+        int lossCount = 0;
+        float avgLossRate = 0;
+        if(indexDatas.size() > 0){
+            init = indexDatas.get(0).getClosePoint();
+        }
+
+        for(int i = 0; i < indexDatas.size(); i++) {
+            float avg = getMA(i, 20, indexDatas);
+            float max = getMax(i, 20, indexDatas);
+            float closePoint = indexDatas.get(i).getClosePoint();
+            float increaseRate = closePoint / avg;
+            float decreaseRate = closePoint / max;
+            if (avg != 0) {
+                if (increaseRate > buyRate) {
+                    if (share == 0) {
+                        share = cash / closePoint;
+                        cash = 0;
+
+                        Trade trade = new Trade();
+                        trade.setBuyDate(indexDatas.get(i).getDate());
+                        trade.setBuyClosePoint(indexDatas.get(i).getClosePoint());
+                        trade.setSellClosePoint(0);
+                        trade.setSellDate("n/a");
+                        trades.add(trade);
+                    }
+                } else if (decreaseRate < sellRate) {
+                    if (cash == 0) {
+                        cash = share * closePoint * (1 - serviceCharge);
+                        share = 0;
+
+                       Trade trade = trades.get(trades.size()-1);
+                       trade.setSellDate(indexDatas.get(i).getDate());
+                       trade.setSellClosePoint(indexDatas.get(i).getClosePoint());
+                       trade.setRate(cash/initCash);
+
+                       if(trade.getSellClosePoint() - trade.getBuyClosePoint() >0){
+                           totalWinRate += (trade.getSellClosePoint() - trade.getBuyClosePoint())/trade.getBuyClosePoint();
+                           winCount++;
+                       }else {
+                           totalLossRate += (trade.getSellClosePoint() - trade.getBuyClosePoint())/trade.getBuyClosePoint();
+                           lossCount++;
+                       }
+                    }
+                }
+            }
+                if (share == 0) {
+                    value = cash;
+                } else {
+                    value = share * closePoint;
+                }
+
+
+
+
+                float rate = value / initCash;
+                Profit p = new Profit();
+                p.setDate(indexDatas.get(i).getDate());
+                p.setValue(rate * init);
+                Profits.add(p);
+        }
+        Profits.stream().forEach(x -> System.out.println(x));
+        avgLossRate = totalLossRate/lossCount;
+        avgWinRate = totalWinRate/winCount;
+        Map<String,Object> map = new HashMap<>();
+        map.put("profits", Profits);
+        map.put("trades",trades);
+
+        map.put("winCount", winCount);
+        map.put("lossCount", lossCount);
+        map.put("avgWinRate", avgWinRate);
+        map.put("avgLossRate", avgLossRate);
+
+        return map;
+    }
+
+    private static float getMax(int i, int day, List<IndexData> list){
+        int start = i-1-day;
+        int end = i-1;
+        if(start <0){
+            start = 0;
+        }
+        float max = 0;
+        if(start < 0){
+            return  0;
+        }
+
+        for(int j = start; j < end; j++){
+            if(max < list.get(j).getClosePoint()){
+                max = list.get(j).getClosePoint();
+            }
+        }
+        return max;
+    }
+    private static float getMA(int i, int ma, List<IndexData> list){
+        int start = i-1-ma;
+        int end = i-1;
+        float sum = 0;
+        float avg = 0;
+
+        if(start < 0){
+            return 0;
+        }
+        for(int j = start; j < end; j++){
+            sum += list.get(j).getClosePoint();
+        }
+        avg = sum/ma;
+        return avg;
+    }
+
+    public float getYear(List<IndexData> list){
+        Date startDate = DateUtil.parse(list.get(0).getDate());
+        Date endDate = DateUtil.parse(list.get(list.size()-1).getDate());
+
+        return DateUtil.between(startDate,endDate,DateUnit.DAY)/365f;
+
     }
 }
